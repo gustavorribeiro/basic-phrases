1659acd7ec5de5507384a4ab593fd303be2c9957
Gustavo Ribeiro <gustavo.mribeiroxg@yahoo.com>
2023-01-24 13:00:48 -0300

    adding playPhrase method

--- app/src/main/java/com/dev/myappcompany/basicphrases/MainActivity.java
+++ app/src/main/java/com/dev/myappcompany/basicphrases/MainActivity.java
@@ -3,8 +3,19 @@ package com.dev.myappcompany.basicphrases;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void playPhrase(View view) {
+
+        Button buttonPressed = (Button) view;
+
+        Log.i("Button pressed", buttonPressed.getTag().toString());
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
